0f1289aa8e4e037b9b40e870c4128decf97be198
hoangdam38it <damthuhoang38it@gmail.com>
2023-06-01 22:28:37 +0700

    Implement Validate Palindrome 1

---
+++ Two-Pointers/ValidPalindrome.js
@@ -0,0 +1,14 @@
+export function isPalindrome(s) {
+    let left = 0;
+    let right = s.length-1;
+    while(left <= right){
+        if(s[left] != s[right]){
+           return false;
+        }
+
+       left++;
+       right--;
+    }
+
+    return true;
+}
